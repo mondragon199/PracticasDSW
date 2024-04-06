f921293dd9d42396e9d51e34ff088f45265607ed
mondragon199 <143614767+mondragon199@users.noreply.github.com>
2024-04-05 21:49:33 -0700

    Se agrego la actividad 24 Alvarado Mondragon

---
+++ practica24/script.js
@@ -0,0 +1,13 @@
+// Rodrigo Moises Alvarado Mondragon
+var hayUnHuracan = true
+function Aviso_De_Huracam(hayUnHuracan) {
+    
+
+    if (hayUnHuracan) {
+        alert("Hay un Huracan");
+    }else{
+        alert("Esta Soleado");
+
+    }
+    
+}
